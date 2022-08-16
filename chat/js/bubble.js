32fa3b849cc214c24a7a773eaacae34b2721e0d9
vlad <vladas272@gmail.com>
2022-08-16 13:01:47 +0300

    updte

--- chat/js/bubble.js
+++ chat/js/bubble.js
@@ -138,19 +138,19 @@ $(".mytext").on("keydown", function (e) {
   }
 });
 
-function createMessage(who, type, text, time) {
-  var message = {
-    who,
-    type,
-    text,
-    time,
-  };
-  // message.who = who;
-  // message.text = text;
-  // message.time = time;
+// function createMessage(who, type, text, time) {
+//   var message = {
+//     who,
+//     type,
+//     text,
+//     time,
+//   };
+//   // message.who = who;
+//   // message.text = text;
+//   // message.time = time;
 
-  return message;
-}
+//   return message;
+// }
 
 function createQuiz(
   question,
@@ -170,200 +170,239 @@ function createQuiz(
 }
 
 let messages = [
-  createMessage(
-    "",
-    "text",
-    "Įlipęs į autobusą, radau mėgstamiausią laisvą vietą prie lango.",
-    0
-  ),
-  createMessage(
-    "",
-    "img", "https://lh3.googleusercontent.com/EdHuRKLfm9CPFJmvwke0ilS42FDEqKkxRXR9RlDwIf_6cRsEsWek_KG7Vf5Fd227iFZbJE6fi7vxhOGAoUwLyvD08msAV3hUTgzfXhuW7bouHYAGhQ4Bc0Y01_sejUk_BBIvRkEmWw=w2400",
-    1500
-  ),
-  createMessage("", "text", "Sekmadieniais ji visąlaik tuščia.", 0),
-  createMessage(
-    "",
-    "text",
-    "Išvyka prie ežero, kur jaučiuosi laisvas ir nepriklausomas, dažniausiai būna trumpa.",
-    0
-  ),
-  createMessage(
-    "",
-    "text",
-    "Todėl su savim tik sumuštiniai, telefonas ir žinoma gera nuotaika.",
-    3500
-  ),
-  createMessage("Vairtuotojas", "text", "Na ką, važiuojam!", 1500
-  ),
-  createMessage(
-    "",
-    "text",
-    "Pro langą pradėjo skrieti vaizdai. Bet šiandien langas, nebuvo mano dėmesio centre.",
-    3500
-  ),
-  createMessage(
-    "",
-    "text",
-    "Nugirdau visai šalia savęs, dviejų, arti vienas kito sėdinčių žmonių pokalbį, tai buvo pora. ",
-    3500
-  ),
-  createMessage(
-    "",
-    "text",
-    "O tu paklausyk brangioji, kokia įdomi istorija su manimi nutiko.  ",
-    3500
-  ),
-  createMessage("Mergina", "text", "Kas galėjo būti svarbiau už mane?  ", 3500),
-  createMessage(
-    "",
-    "text",
-    "Už Tave? Tikrai niekas, bet Tu tik paklausyk. ",
-    3500
-  ),
-  createMessage(
-    "",
-    "text",
-    "Vakar dieną, atsitiktinai tapau liudininku įdomaus įvykio.",
-    3500
-  ),
-  createMessage("Mergina", "text", "Aš klausau…", 1500),
-  createMessage(
-    "",
-    "text",
-    "Atsidūriau erdvioje ir patogioje salėje, o ten visą laiką spindėjo damos. Kiekviename salės taške jautėsi pozityvi energija, pasitikėjimas ir patikimumas. ",
-    3500
-  ),
-  createMessage("Mergina", "text", "Damos?", 3500),
-  createMessage(
-    "",
-    "text",
-    "Aš pagalvojau, jog kas turi akis tegul mato! Kas turi ausis tegul klauso!",
-    3500
-  ),
-  createMessage(
-    "Mergina",
-    "text",
-    "Ir Tu tikriausiai viską matei ir viską klausei? ",
-    1500
-  ),
-  createMessage("", "text", "Taip, tiesa.", 3500),
-  createMessage(
-    "",
-    "text",
-    "Žinok keisčiausia, jog kaip žaibas iš giedro dangaus salės centre pamačiau ją.",
-    3500
-  ),
-  createMessage(
-    "",
-    "text",
-    "Mergina šiek tiek pašiurpo, jos veidą persmelkė nuostabos kibirkštis. Ji stipriau suspaudė vaikinui ranką. ",
-    1500
-  ),
-  createMessage(
-    "",
-    "text",
-    "Ji buvo nuostabi. Aplink ją buriavosi žmonės. ",
-    3500
-  ),
-  createMessage("", "text", "Tikra ledi, dama, karalienė. ", 3500),
-  createMessage(
-    "",
-    "text",
-    "Diplomatiškai įsimylėjau iš pirmo žvilgsnio.",
-    3500
-  ),
-  createMessage("Mergina", "text", "Ką? Kas ji, greit sakyk! ", 3500),
-  createMessage(
-    "",
-    "text",
-    "Tai apie ką dabar kalbu, vadinama knyga, mano brangioji.",
-    3500
-  ),
-  createMessage("Mergina", "text", "Kaip, knyga?  ", 3500),
-  createMessage(
-    "",
-    "text",
-    "Taip, buvo renginys parduotuvėje knygų klubas, kai atidarė praeivis duris, nesusilaikiau nuo to kvapo, naujos išleistos knygos kvapo.",
-    3500
-  ),
-  createMessage(
-    "Mergina",
-    "text",
-    "Na taip, kažkurie bėgioja paskui sijonus, o Tu pasirinkai knygas… ",
-    3500
-  ),
-  createMessage(
-    "",
-    "text",
-    "Skaitai, jog turėčiau labiau domėtis merginomis?",
-    3500
-  ),
-  createMessage("Mergina", "text", "Ne! Tuoj aš tau… ", 3500),
-  createMessage(
-    "",
-    "text",
-    "Aš sustojau kitoje stotelėje, mane kažkaip sužavėjo jaunuolio pasakojimas.",
-    3500
-  ),
-  createMessage(
-    "",
-    "text",
-    "Pagal koordinatės, knygų klubas turėtu būti visai šalia, gaila aš taip gerai nejaučiu knygų kvapo, kaip tas vaikinas.",
-    3500
-  ),
-  createMessage(
-    "",
-    "img",
-    "https://lh3.googleusercontent.com/Vod4sjqQUtxJIuunmdjX0_H92uLjp7CQ6i9fnLZz4HxAftpcybygNQUatNWYBv_lGPBL8DfOXgGwV3Ea6T7pDlkJsmagSWuWcIMvfRKeno1Ol2QYshDNnKY4HRkiU-gUL98NBqVnEw=w2400",
-    1500
-  ),
-  createMessage(
-    "Pardavėja",
-    "text",
-    "Laba diena, prašome užeiti, nesidrovėkite.",
-    3500
-  ),
-  createMessage(
-    "",
-    "text",
-    "Norėčiau įsigyti šiuo metu populiariausią knygą.",
-    3500
-  ),
-  createMessage(
-    "Pardavėja",
-    "text",
-    "Tai būtų, “Ten, kur gieda vėžiai” Delia Owens.",
-    3500
-  ),
-  createMessage(
-    "",
-    "img",
-    "https://lh3.googleusercontent.com/VFFUVkIQ4If2yBsmmKpRzahK2z6gzDf_G5Ow33PXZ2Yn_CBzRfdf58z0lMO8xiByEffzxj0niE1WDUuoADfB5oZaxJC_UzythbOnJKCMhKq0t1Et7IickGiGWgNnF01n0CW7O9XUyA=w2400",
-    1500
-  ),
-  createMessage(
-    "Pardavėja",
-    "text",
-    "Pasakojimas apie apleistą ir izoliuotą nuo visų merginą, vardu Kaja, kuri išmoksta pragyventi iš to, ką siūlo motina gamta.",
-    3500
-  ),
-  createMessage("", "text", "Visai skamba įdomiai.", 3500),
-  createMessage(
-    "Pardavėja",
-    "text",
-    "Taip, liko vos keli vienetai, ją perka kaip išprotėję, jums įdėti į paprastą maišelį ar tai būtų kam nors dovana?",
-    3500
-  ),
-  createMessage("", "text", "Prašau į paprastą maišelį.", 3500),
-  createMessage("Pardavėja", "text", "Geros Jums dienos ir skaitymo.", 3500),
-  createMessage(
-    "",
-    "text",
-    "Tikriausiai šiandien išvyka mano palauks, nelabai mėgstu skaityt knygas gamtoje, reikia grįžti namo išsijungti visas priemones, kurios trukdys.",
-    3500
-  ),
-  createMessage("", "text", "Gero man skaitymo", 3500),
+  {
+    who: "",
+    type: "text",
+    text: "Įlipęs į autobusą, radau mėgstamiausią laisvą vietą prie lango.",
+    time: 0,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Sekmadieniais ji visąlaik tuščia.",
+    time: 0,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Išvyka prie ežero, kur jaučiuosi laisvas ir nepriklausomas, dažniausiai būna trumpa.",
+    time: 0,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Todėl su savim tik sumuštiniai, telefonas ir žinoma gera nuotaika.",
+    time: 3500,
+  },
+  { who: "Vairtuotojas", type: "text", text: "Na ką, važiuojam!", time: 1500 },
+  {
+    who: "Vairtuotojas",
+    type: "img",
+    text: "https://i.pinimg.com/originals/11/03/fc/1103fc76b82d5b29e66fddffbab6a84c.jpg",
+    time: 1500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Pro langą pradėjo skrieti vaizdai. Bet šiandien langas, nebuvo mano dėmesio centre.",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Nugirdau visai šalia savęs, dviejų, arti vienas kito sėdinčių žmonių pokalbį, tai buvo pora. ",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "img",
+    text: "https://media1.popsugar-assets.com/files/thumbor/dLc8-rDFJRbZYlhpqH29Qh2mgw4/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/25/699/n/1922283/06adea33772b2923_MCDONUP_EC176/i/Once-Upon-Time-Hollywood-Movie-Posters.jpg",
+    time: 1500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "O tu paklausyk brangioji, kokia įdomi istorija su manimi nutiko.  ",
+    time: 3500,
+  },
+  {
+    who: "Mergina",
+    type: "text",
+    text: "Kas galėjo būti svarbiau už mane?",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Už Tave? Tikrai niekas, bet Tu tik paklausyk. ",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Vakar dieną, atsitiktinai tapau liudininku įdomaus įvykio.",
+    time: 3500,
+  },
+  {
+    who: "Mergina",
+    type: "text",
+    text: "Aš klausau…",
+    time: 1500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Atsidūriau erdvioje ir patogioje salėje, o ten visą laiką spindėjo damos. Kiekviename salės taške jautėsi pozityvi energija, pasitikėjimas ir patikimumas. ",
+    time: 3500,
+  },
+  {
+    who: "Mergina",
+    type: "text",
+    text: "Damos?",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Aš pagalvojau, jog kas turi akis tegul mato! Kas turi ausis tegul klauso!",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Ir Tu tikriausiai viską matei ir viską klausei? ",
+    time: 1500,
+  },
+  { who: "", type: "text", text: "Taip, tiesa.", time: 3500 },
+  {
+    who: "",
+    type: "text",
+    text: "Žinok keisčiausia, jog kaip žaibas iš giedro dangaus salės centre pamačiau ją.",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Mergina šiek tiek pašiurpo, jos veidą persmelkė nuostabos kibirkštis. Ji stipriau suspaudė vaikinui ranką. ",
+    time: 1500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Ji buvo nuostabi. Aplink ją buriavosi žmonės. ",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Tikra ledi, dama, karalienė. ",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Diplomatiškai įsimylėjau iš pirmo žvilgsnio.",
+    time: 3500,
+  },
+  {
+    who: "Mergina",
+    type: "text",
+    text: "Ką? Kas ji, greit sakyk! ",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Tai apie ką dabar kalbu, vadinama knyga, mano brangioji.",
+    time: 3500,
+  },
+  {
+    who: "Mergina",
+    type: "text",
+    text: "Kaip, knyga?  ",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Taip, buvo renginys parduotuvėje knygų klubas, kai atidarė praeivis duris, nesusilaikiau nuo to kvapo, naujos išleistos knygos kvapo.",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Na taip, kažkurie bėgioja paskui sijonus, o Tu pasirinkai knygas… ",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Skaitai, jog turėčiau labiau domėtis merginomis?",
+    time: 3500,
+  },
+  {
+    who: "Mergina",
+    type: "text",
+    text: "Ne! Tuoj aš tau… ",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Aš sustojau kitoje stotelėje, mane kažkaip sužavėjo jaunuolio pasakojimas.",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Pagal koordinatės, knygų klubas turėtu būti visai šalia, gaila aš taip gerai nejaučiu knygų kvapo, kaip tas vaikinas.",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Laba diena, prašome užeiti, nesidrovėkite.",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Norėčiau įsigyti šiuo metu populiariausią knygą.",
+    time: 3500,
+  },
+  {
+    who: "Pardavėja",
+    type: "text",
+    text: "Tai būtų, “Ten, kur gieda vėžiai” Delia Owens.",
+    time: 3500,
+  },
+  {
+    who: "Pardavėja",
+    type: "text",
+    text: "Pasakojimas apie apleistą ir izoliuotą nuo visų merginą, vardu Kaja, kuri išmoksta pragyventi iš to, ką siūlo motina gamta.",
+    time: 3500,
+  },
+  { who: "", type: "text", text: "Visai skamba įdomiai.", time: 3500 },
+  {
+    who: "Pardavėja",
+    type: "text",
+    text: "Taip, liko vos keli vienetai, ją perka kaip išprotėję, jums įdėti į paprastą maišelį ar tai būtų kam nors dovana?",
+    time: 3500,
+  },
+  { who: "", type: "text", text: "Prašau į paprastą maišelį.", time: 3500 },
+  {
+    who: "Pardavėja",
+    type: "text",
+    text: "Geros Jums dienos ir skaitymo.",
+    time: 3500,
+  },
+  {
+    who: "",
+    type: "text",
+    text: "Tikriausiai šiandien išvyka mano palauks, nelabai mėgstu skaityt knygas gamtoje, reikia grįžti namo išsijungti visas priemones, kurios trukdys.",
+    time: 3500,
+  },
+  { who: "", type: "text", text: "Gero man skaitymo", time: 3500 },
 ];
 
 let totalMessages = messages.length;
@@ -375,8 +414,8 @@ $("body").click(function () {
     let msg = messages.shift();
     document.getElementById("welcome").style.display = "none";
     if (!isQuiz) {
-      if (numberOfMessages > maxMessageCount)
-        elem = $(".msg-container:first").first().remove();
+      // if (numberOfMessages > maxMessageCount)
+      //   elem = $(".msg-container:first").first().remove();
       if (msg.hasOwnProperty("who")) {
         insertChat(msg.who, msg.type, msg.text, msg.time);
       } else {
